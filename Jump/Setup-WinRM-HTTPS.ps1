56b2392421ea3bd520ee3afe5c15c669fbeff823
HenriBorsboom <henri.borsboom@gmail.com>
2025-07-17 09:15:10 +0200

    Added files
    Added files

---
+++ Jump/Setup-WinRM-HTTPS.ps1
@@ -0,0 +1,5 @@
+New-SelfSignedCertificate -DnsName jump01.ansible.local -CertStoreLocation Cert:\LocalMachine\My
+$thumb = (Get-ChildItem Cert:\LocalMachine\My  | Where-Object Subject -like '*jump01.ansible.local*').Thumbprint
+winrm delete winrm/config/Listener?Address=*+Transport=HTTPS
+winrm create winrm/config/Listener?Address=*+Transport=HTTPS "@{Hostname=`"jump01.ansible.local`";CertificateThumbprint=`"$thumb`"}"
+Restart-Service WinRM
